6ce6a0e7e1d767d9999272b0490f98a09666bcc1
iyear <ljyngup@gmail.com>
2022-05-16 11:49:58 +0800

    update(example): add new handlers

--- examples/plus/plugin/plugin.go
+++ examples/plus/plugin/plugin.go
@@ -8,12 +8,17 @@ import (
 )
 
 func main() {
-	p := plugin.New("math", "v1", "123",
+	p := plugin.New("MyPlugin", "v1", "123",
 		plugin.WithLogLevel(plugin.LogLevelDebug),
 		plugin.WithHeartbeat(10*time.Second),
 		plugin.WithDialOpts(grpc.WithInsecure()))
 
+	// if handle only plus, plugin can't bind to core because it doesn't impl any interface
+	// if handle plus and multiply, plugin can bind to core
+	// if handle plus, multiply and echo, plugin can't bind to core because it impls two interfaces
 	p.Handle("plus", plus)
+	p.Handle("multiply", multiply)
+	p.Handle("echo", echo)
 
 	if err := p.Mount("localhost", 13001); err != nil {
 		log.Println(err)
@@ -30,5 +35,23 @@ func plus(ctx plugin.Context) (map[string]interface{}, error) {
 
 	return map[string]interface{}{
 		"V": args["A"].(float64) + args["B"].(float64),
+	}, nil
+}
+
+func multiply(ctx plugin.Context) (map[string]interface{}, error) {
+	ctx.L().Info("enter math.v1.multiply")
+	args := ctx.Args()
+	ctx.L().Info("finish multiply func")
+
+	return map[string]interface{}{
+		"V": args["A"].(float64) * args["B"].(float64),
+	}, nil
+}
+
+func echo(ctx plugin.Context) (map[string]interface{}, error) {
+	text := ctx.Args()["Text"].(string)
+	//ctx.L().Debugf("echo %s", text)
+	return map[string]interface{}{
+		"Text": text,
 	}, nil
 }
